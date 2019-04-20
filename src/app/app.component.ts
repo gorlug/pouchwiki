fc65bb9cce16bc33c12625125554772f24cc67b4
Achim Rohn <achim@rohn.eu>
2019-04-20 18:58:52 +0200

    Render html from markdown using marked
    Issue #1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component} from "@angular/core";
+import * as marked from "marked";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+    selector: "app-root",
+    templateUrl: "./app.component.html",
+    styleUrls: ["./app.component.sass"]
 })
 export class AppComponent {
-  title = 'pouchwiki';
+    title = "pouchwiki";
+
+    html: string;
+
+    constructor() {
+        this.html = marked(`# Welcome to PouchWiki!
+
+Rendered with marked.
+
+* item1
+* item2
+
+[link](http://google.com)
+`);
+    }
 }
