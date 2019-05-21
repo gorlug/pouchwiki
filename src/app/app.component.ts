2698c28e5d89b6d62f74161719db723ce2c03e43
Achim Rohn <achim@rohn.eu>
2019-05-22 00:01:56 +0200

    Add initial dynamic theming code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import {LoginService} from "./login.service";
 import {LoggingService} from "./logging.service";
 import {PageService} from "./page.service";
 import {ActivatedRoute} from "@angular/router";
+import {BehaviorSubject} from "rxjs";
 
 const LOG_NAME = "AppComponent";
 
@@ -13,6 +14,8 @@ const LOG_NAME = "AppComponent";
 })
 export class AppComponent implements OnInit {
     title = "pouchwiki";
+
+    isDarkTheme = new BehaviorSubject(true);
 
     constructor(public loginService: LoginService,
                 private loggingService: LoggingService,
